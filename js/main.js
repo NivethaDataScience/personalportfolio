03547de424e2601cf9ab4e75cc950f00f9f17ab6
NivethaDataScience <knivetha2453@gmail.com>
2024-07-15 21:56:59 +0530

    Editted

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:["Data Scientist","Data Analyst","AI Researcher",],
+    strings:["Data Scientist","Data Analyst","AI Researcher","ML"],
     typeSpeed:50,
     backSpeed:30,
     loop:true
